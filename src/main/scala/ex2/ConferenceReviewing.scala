d429df366cd84ee633fd6dda7ac4b296ba900505
Alex Testa <alex.testa@studio.unibo.it>
2024-04-18 18:14:22 +0200

    Bug fix

--- src/main/scala/ex2/ConferenceReviewing.scala
+++ src/main/scala/ex2/ConferenceReviewing.scala
@@ -22,7 +22,7 @@ object ConferenceReviewing:
         override def loadReview(article: Int, scores: Map[Question, Int]): Unit = reviews = reviews.::((article, scores))
 
         override def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int) : Unit =
-          reviews = reviews.::((article, Map.apply((Question.RELEVANCE, relevance), (Question.SIGNIFICANCE, significance), (Question.CONFIDENCE, confidence), (Question.FINAL, fin))))
+          reviews = reviews.::((article, Map((Question.RELEVANCE, relevance), (Question.SIGNIFICANCE, significance), (Question.CONFIDENCE, confidence), (Question.FINAL, fin))))
 
         override def orderedScores(article: Int, question: Question): List[Int] =
           reviews.filter(a => a._1 == article).map(a => a._2(question)).sorted
@@ -38,4 +38,4 @@ object ConferenceReviewing:
           val acceptedArticles = this.acceptedArticles() ; reviews.filter(a => acceptedArticles.contains(a._1)).map(a => (a._1, averageFinalScore(a._1))).distinct.sortBy(_._2)
 
         override def averageWeightedFinalScoreMap(): Map[Int, Double] =
-          reviews.map(i => (i._1, (averageScore(i._1, CONFIDENCE) * averageFinalScore(i._1)) / (reviews.size - 1))).toMap
+          reviews.map(i => (i._1, (averageScore(i._1, CONFIDENCE) * averageFinalScore(i._1)) / 10.0)).toMap
