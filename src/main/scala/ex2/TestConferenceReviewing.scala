a8ddb1fdb5f64c131188e4bca785154fe328c93a
Alex Testa <alex.testa@studio.unibo.it>
2024-04-18 11:33:42 +0200

    Fix test

--- src/main/scala/ex2/TestConferenceReviewing.scala
+++ src/main/scala/ex2/TestConferenceReviewing.scala
@@ -15,41 +15,46 @@ class TestConferenceReviewing:
     cr.loadReview(3, 4, 4, 4, 4)
     cr.loadReview(4, 6, 6, 6, 6)
     cr.loadReview(4, 7, 7, 8, 7)
-    val map = Map.apply((Question.RELEVANCE, 8), (Question.SIGNIFICANCE, 8), (Question.CONFIDENCE, 7), (Question.FINAL, 8))
+    val map = Map.apply(
+      (Question.RELEVANCE, 8),
+      (Question.SIGNIFICANCE, 8),
+      (Question.CONFIDENCE, 7),
+      (Question.FINAL, 8)
+    )
     cr.loadReview(4, map)
     cr.loadReview(5, 6, 6, 6, 10)
     cr.loadReview(5, 7, 7, 7, 10)
 
   @Test def orderedScores(): Unit =
     init()
-    assertEquals(cr.orderedScores(2, Question.RELEVANCE), List.apply(4, 9))
-    assertEquals(cr.orderedScores(4, Question.CONFIDENCE), List.apply(6, 7, 8))
-    assertEquals(cr.orderedScores(5, Question.FINAL), List.apply(10, 10))
+    assertEquals(List.apply(4, 9), cr.orderedScores(2, Question.RELEVANCE))
+    assertEquals(List.apply(6, 7, 8), cr.orderedScores(4, Question.CONFIDENCE))
+    assertEquals(List.apply(10, 10), cr.orderedScores(5, Question.FINAL))
 
   @Test def testAverageFinalScore(): Unit =
     init()
-    assertEquals(cr.averageFinalScore(1), 8.5, 0.01);
-    assertEquals(cr.averageFinalScore(2), 7.5, 0.01);
-    assertEquals(cr.averageFinalScore(3), 3.5, 0.01);
-    assertEquals(cr.averageFinalScore(4), 7.0, 0.01);
-    assertEquals(cr.averageFinalScore(5), 10.0, 0.01);
+    assertEquals(8.5, cr.averageFinalScore(1), 0.01);
+    assertEquals(7.5, cr.averageFinalScore(2), 0.01);
+    assertEquals(3.5, cr.averageFinalScore(3), 0.01);
+    assertEquals(7.0, cr.averageFinalScore(4), 0.01);
+    assertEquals(10.0, cr.averageFinalScore(5), 0.01);
 
   @Test def testAcceptedArticles(): Unit =
     init()
-    assertEquals(cr.acceptedArticles(), Set.apply(1, 2, 4))
+    assertEquals(Set.apply(1, 2, 4), cr.acceptedArticles())
 
   @Test def testSortedAcceptedArticles(): Unit =
     init()
     assertEquals(
-      cr.sortedAcceptedArticles(),
-      List.apply((4, 7.0), (2, 7.5), (1, 8.5))
+      List.apply((4, 7.0), (2, 7.5), (1, 8.5)),
+      cr.sortedAcceptedArticles()
     )
 
   @Test def optionalTestAverageWeightedFinalScore(): Unit =
     init()
-    assertEquals(cr.averageWeightedFinalScoreMap().get(1), (4.8 + 5.4) / 2)
-    assertEquals(cr.averageWeightedFinalScoreMap().get(2), (9.0 + 6.0) / 2)
-    assertEquals(cr.averageWeightedFinalScoreMap().get(3), (0.9 + 1.6) / 2)
-    assertEquals(cr.averageWeightedFinalScoreMap().get(4), (3.6 + 5.6 + 5.6) / 3)
-    assertEquals(cr.averageWeightedFinalScoreMap().get(5), (6.0 + 7.0) / 2)
-    assertEquals(cr.averageWeightedFinalScoreMap().size, 5)
+    assertEquals((4.8 + 5.4) / 2, cr.averageWeightedFinalScoreMap().get(1))
+    assertEquals((9.0 + 6.0) / 2, cr.averageWeightedFinalScoreMap().get(2))
+    assertEquals((0.9 + 1.6) / 2, cr.averageWeightedFinalScoreMap().get(3))
+    assertEquals((3.6 + 5.6 + 5.6) / 3, cr.averageWeightedFinalScoreMap().get(4))
+    assertEquals((6.0 + 7.0) / 2, cr.averageWeightedFinalScoreMap().get(5))
+    assertEquals(5, cr.averageWeightedFinalScoreMap().size)
