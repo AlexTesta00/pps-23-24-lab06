4bbd67244794f57627f782401858626965703c75
Alex Testa <alex.testa@studio.unibo.it>
2024-04-18 16:42:08 +0200

    Finish exercise 2

--- src/main/scala/ex2/ConferenceReviewing.scala
+++ src/main/scala/ex2/ConferenceReviewing.scala
@@ -1,5 +1,7 @@
 package ex2
 
+import ex2.Question.{CONFIDENCE, FINAL, RELEVANCE}
+
 enum Question:
     case RELEVANCE, SIGNIFICANCE, CONFIDENCE, FINAL
 
@@ -8,7 +10,7 @@ trait ConferenceReviewing:
     def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int) : Unit
     def orderedScores(article: Int, question: Question): List[Int]
     def averageFinalScore(article: Int): Double
-    def acceptedArticles(): Set[Integer]
+    def acceptedArticles(): Set[Int]
     def sortedAcceptedArticles(): List[(Int, Double)]
     def averageWeightedFinalScoreMap(): Map[Int, Double]
 
@@ -16,10 +18,32 @@ object ConferenceReviewing:
     def apply(): ConferenceReviewing = ConferenceReviewingImpl()
 
     private case class ConferenceReviewingImpl() extends ConferenceReviewing:
-        override def loadReview(article: Int, scores: Map[Question, Int]) : Unit = ???
-        override def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int) : Unit = ???
-        override def orderedScores(article: Int, question: Question): List[Int] = ???
-        override def averageFinalScore(article: Int): Double = ??? 
-        override def acceptedArticles(): Set[Integer] = ???
-        override def sortedAcceptedArticles(): List[(Int, Double)] = ???
-        override def averageWeightedFinalScoreMap(): Map[Int, Double] = ???
+      private var reviews = List[(Int, Map[Question, Int])]()
+        override def loadReview(article: Int, scores: Map[Question, Int]): Unit =
+          reviews = reviews.::((article, scores))
+        override def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int) : Unit =
+          reviews = reviews.::((
+            article,
+            Map.apply(
+              (Question.RELEVANCE, relevance),
+              (Question.SIGNIFICANCE, significance),
+              (Question.CONFIDENCE, confidence),
+              (Question.FINAL, fin)
+            )
+          ))
+        override def orderedScores(article: Int, question: Question): List[Int] =
+          reviews.filter(a => a._1 == article).map(a => a._2.get(question)).map(i => i.get).sorted
+
+        override def averageFinalScore(article: Int): Double = averageScore(article, FINAL)
+
+        private def averageScore(article: Int, question: Question): Double =
+          val scores = orderedScores(article, question)
+          scores.sum.doubleValue / scores.size
+
+        override def acceptedArticles(): Set[Int] =
+          reviews.filter(_._2(Question.RELEVANCE) >= 8).filter(a => averageFinalScore(a._1) > 5).map(i => i._1).toSet
+        override def sortedAcceptedArticles(): List[(Int, Double)] =
+          val acceptedArticles = this.acceptedArticles()
+          reviews.filter(a => acceptedArticles.contains(a._1)).map(a => (a._1, averageFinalScore(a._1))).distinct.sortBy(_._2)
+        override def averageWeightedFinalScoreMap(): Map[Int, Double] =
+          reviews.map(i => (i._1, (averageScore(i._1, CONFIDENCE) * averageFinalScore(i._1)) / 10.0)).toMap
--- src/main/scala/ex2/TestConferenceReviewing.scala
+++ src/main/scala/ex2/TestConferenceReviewing.scala
@@ -6,7 +6,7 @@ import org.junit.Assert.*
 class TestConferenceReviewing:
   private val cr = ConferenceReviewing()
 
-  private def init(): Unit =
+  @Before def init(): Unit =
     cr.loadReview(1, 8, 8, 6, 8)
     cr.loadReview(1, 9, 9, 6, 9)
     cr.loadReview(2, 9, 9, 10, 9)
@@ -26,35 +26,32 @@ class TestConferenceReviewing:
     cr.loadReview(5, 7, 7, 7, 10)
 
   @Test def orderedScores(): Unit =
-    init()
     assertEquals(List.apply(4, 9), cr.orderedScores(2, Question.RELEVANCE))
     assertEquals(List.apply(6, 7, 8), cr.orderedScores(4, Question.CONFIDENCE))
     assertEquals(List.apply(10, 10), cr.orderedScores(5, Question.FINAL))
 
   @Test def testAverageFinalScore(): Unit =
-    init()
-    assertEquals(8.5, cr.averageFinalScore(1), 0.01);
-    assertEquals(7.5, cr.averageFinalScore(2), 0.01);
-    assertEquals(3.5, cr.averageFinalScore(3), 0.01);
-    assertEquals(7.0, cr.averageFinalScore(4), 0.01);
-    assertEquals(10.0, cr.averageFinalScore(5), 0.01);
+    val delta = 0.01
+    assertEquals(8.5, cr.averageFinalScore(1), delta)
+    assertEquals(7.5, cr.averageFinalScore(2), delta)
+    assertEquals(3.5, cr.averageFinalScore(3), delta)
+    assertEquals(7.0, cr.averageFinalScore(4), delta)
+    assertEquals(10.0, cr.averageFinalScore(5), delta)
 
   @Test def testAcceptedArticles(): Unit =
-    init()
     assertEquals(Set.apply(1, 2, 4), cr.acceptedArticles())
 
   @Test def testSortedAcceptedArticles(): Unit =
-    init()
     assertEquals(
       List.apply((4, 7.0), (2, 7.5), (1, 8.5)),
       cr.sortedAcceptedArticles()
     )
 
   @Test def optionalTestAverageWeightedFinalScore(): Unit =
-    init()
-    assertEquals((4.8 + 5.4) / 2, cr.averageWeightedFinalScoreMap().get(1))
-    assertEquals((9.0 + 6.0) / 2, cr.averageWeightedFinalScoreMap().get(2))
-    assertEquals((0.9 + 1.6) / 2, cr.averageWeightedFinalScoreMap().get(3))
-    assertEquals((3.6 + 5.6 + 5.6) / 3, cr.averageWeightedFinalScoreMap().get(4))
-    assertEquals((6.0 + 7.0) / 2, cr.averageWeightedFinalScoreMap().get(5))
+    val delta = 0.1
+    assertEquals((4.8 + 5.4) / 2, cr.averageWeightedFinalScoreMap()(1), delta)
+    assertEquals((9.0 + 6.0) / 2, cr.averageWeightedFinalScoreMap()(2), delta)
+    assertEquals((0.9 + 1.6) / 2, cr.averageWeightedFinalScoreMap()(3), delta)
+    assertEquals((3.6 + 5.6 + 5.6) / 3, cr.averageWeightedFinalScoreMap()(4), delta)
+    assertEquals((6.0 + 7.0) / 2, cr.averageWeightedFinalScoreMap()(5), delta)
     assertEquals(5, cr.averageWeightedFinalScoreMap().size)
