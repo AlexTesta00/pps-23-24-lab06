d1582d20612a11e0b9f9382508f43addfb8f55c2
Alex Testa <alex.testa@studio.unibo.it>
2024-04-18 17:27:07 +0200

    Refactor

--- src/main/scala/ex2/ConferenceReviewing.scala
+++ src/main/scala/ex2/ConferenceReviewing.scala
@@ -19,32 +19,23 @@ object ConferenceReviewing:
 
     private case class ConferenceReviewingImpl() extends ConferenceReviewing:
       private var reviews = List[(Int, Map[Question, Int])]()
-        override def loadReview(article: Int, scores: Map[Question, Int]): Unit =
-          reviews = reviews.::((article, scores))
+        override def loadReview(article: Int, scores: Map[Question, Int]): Unit = reviews = reviews.::((article, scores))
+
         override def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int) : Unit =
-          reviews = reviews.::((
-            article,
-            Map.apply(
-              (Question.RELEVANCE, relevance),
-              (Question.SIGNIFICANCE, significance),
-              (Question.CONFIDENCE, confidence),
-              (Question.FINAL, fin)
-            )
-          ))
+          reviews = reviews.::((article, Map.apply((Question.RELEVANCE, relevance), (Question.SIGNIFICANCE, significance), (Question.CONFIDENCE, confidence), (Question.FINAL, fin))))
+
         override def orderedScores(article: Int, question: Question): List[Int] =
-          reviews.filter(a => a._1 == article).map(a => a._2.get(question)).map(i => i.get).sorted
+          reviews.filter(a => a._1 == article).map(a => a._2(question)).sorted
 
         override def averageFinalScore(article: Int): Double = averageScore(article, FINAL)
 
-        private def averageScore(article: Int, question: Question): Double =
-          val scores = orderedScores(article, question)
-          scores.sum.doubleValue / scores.size
+        private def averageScore(article: Int, question: Question): Double = 
+          val scores = orderedScores(article, question) ; scores.sum.doubleValue / scores.size
 
-        override def acceptedArticles(): Set[Int] =
-          reviews.filter(a => a._2(Question.RELEVANCE) >= 8 && averageFinalScore(a._1) > 5).map(i => i._1).toSet
+        override def acceptedArticles(): Set[Int] = reviews.filter(a => a._2(Question.RELEVANCE) >= 8 && averageFinalScore(a._1) > 5).map(i => i._1).toSet
 
         override def sortedAcceptedArticles(): List[(Int, Double)] =
-          val acceptedArticles = this.acceptedArticles()
-          reviews.filter(a => acceptedArticles.contains(a._1)).map(a => (a._1, averageFinalScore(a._1))).distinct.sortBy(_._2)
+          val acceptedArticles = this.acceptedArticles() ; reviews.filter(a => acceptedArticles.contains(a._1)).map(a => (a._1, averageFinalScore(a._1))).distinct.sortBy(_._2)
+
         override def averageWeightedFinalScoreMap(): Map[Int, Double] =
           reviews.map(i => (i._1, (averageScore(i._1, CONFIDENCE) * averageFinalScore(i._1)) / (reviews.size - 1))).toMap
